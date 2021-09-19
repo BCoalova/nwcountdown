6f8977c913625548e91ce99dd754ace8917aeb89
Boris Coalova <68432860+BCoalova@users.noreply.github.com>
2021-09-19 20:36:11 -0300

    ready

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const countDownNode = document.querySelector('[countdown]')
+
+const newWorldRelease = new Date("Sep 28, 2021 11:00:00").getTime();
+
+const countDown = setInterval( () => {
+    let now = new Date().getTime()
+
+    let distance = newWorldRelease - now
+
+    let day = Math.floor(distance / (1000 * 60 * 60 * 24))
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    countDownNode.innerHTML = `${day} Días, ${hours}h ${minutes}m ${seconds < 10 ? '0' + seconds : seconds}s`
+
+    if (distance < 0) {
+
+        clearInterval(countDown) 
+        countDownNode.innerHTML = 'LETS PLAY'
+
+    }
+
+})
