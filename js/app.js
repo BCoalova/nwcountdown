9a7faa54b6e22edb94281557b93b9bdc124e59e2
Boris Coalova <68432860+BCoalova@users.noreply.github.com>
2021-09-21 11:27:51 -0300

    polish time zones

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const countDownNode = document.querySelector('[countdown]')
 
-const newWorldRelease = new Date("Sep 28, 2021 11:00:00").getTime();
+const newWorldRelease = new Date(Date.UTC(2021, 8, 28, 14, 00, 00)).getTime()
 
-const countDown = setInterval( () => {
+const countDown = setInterval(() => {
     let now = new Date().getTime()
 
     let distance = newWorldRelease - now
@@ -15,10 +15,7 @@ const countDown = setInterval( () => {
     countDownNode.innerHTML = `${day} Días, ${hours}h ${minutes}m ${seconds < 10 ? '0' + seconds : seconds}s`
 
     if (distance < 0) {
-
-        clearInterval(countDown) 
+        clearInterval(countDown)
         countDownNode.innerHTML = 'LETS PLAY'
-
     }
-
 }, 300)
